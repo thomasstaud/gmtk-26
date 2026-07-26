b53ee3e4e3dc370462b63bb596097350bd40e2c7
xENNIxx <enni2004@gmx.at>
2026-07-26 18:18:50 +0200

    abilit

--- ui/controls.gd
+++ ui/controls.gd
@@ -2,9 +2,19 @@ class_name Controls
 extends Node
 
 @onready var dash_cooldown: TextureProgressBar = $DashCooldown
+@onready var bomb: Label = %Bomb
 
+func _ready() -> void:
+	update_visibility()
+
+func update_visibility() -> void:
+	dash_cooldown.visible = AbilityManager.dash.bought
+	bomb.visible = AbilityManager.bomb.bought
 
 func dash(cooldown: int) -> void:
+	if not dash_cooldown.visible:
+		return
+		
 	dash_cooldown.value = 100
 	var tween = get_tree().create_tween()
 	tween.tween_property(dash_cooldown, "value", 0, cooldown)
