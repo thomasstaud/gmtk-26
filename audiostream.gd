9491ce5ae3397d7d8468eacb00b2bdee469b7f4d
xENNIxx <enni2004@gmx.at>
2026-07-26 17:17:00 +0200

    theme

--- audiostream.gd
+++ audiostream.gd
@@ -6,7 +6,7 @@ func _ready() -> void:
 	audio_player = AudioStreamPlayer.new()
 	add_child(audio_player)
 	audio_player.bus = "Master" 
-	play_music("res://audio/music.wav", -22.0)
+	play_music("res://audio/music.wav", -25.0)
 
 func play_music(file_path: String, volume_db: float = 0.0) -> void:
 	if audio_player.stream and audio_player.stream.resource_path == file_path and audio_player.playing:
