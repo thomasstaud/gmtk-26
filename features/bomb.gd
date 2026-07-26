94e092bd0822ffbb8be32deaf9f4c1f0edfedd07
thomasstaud <thomas.staud05@gmail.com>
2026-07-26 18:10:43 +0200

    bomb sound

--- features/bomb.gd
+++ features/bomb.gd
@@ -6,6 +6,7 @@ const DECAY_FACTOR := 0.08
 const DELAY := 2.5
 
 @onready var area: Area3D = $Area3D
+@onready var audio_stream_player_3d: AudioStreamPlayer3D = %AudioStreamPlayer3D
 
 var targets: Array[RigidBody3D] = []
 
@@ -18,7 +19,10 @@ func explode():
 		var base_impulse = vec.normalized() * EXPLOSION_STRENGTH
 		var decay = max(1, (DECAY_FACTOR / vec.length()))
 		body.apply_central_impulse(base_impulse * decay)
-	queue_free()
+	audio_stream_player_3d.play()
+	$bomb2.hide()
+	$MeshInstance3D2.hide()
+	get_tree().create_timer(0.5).timeout.connect(func(): queue_free())
 
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
